3811eca616c12f6d206d7cab8e3e85bd8828b43e
Arthemus <arthemus.moreira@gmail.com>
2019-08-16 15:16:22 +1000

    Testing JSON reading.

--- core/list.js
+++ core/list.js
@@ -1,26 +1,7 @@
 import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { success, failure } from '../libs/response-lib'
 
-const FILL_DATABASE = true
-
-const estimationsData = require('./../estimations.json')
-
 export const main = async () => {
-  if (FILL_DATABASE) {
-    try {
-      console.log('Put all itens on price-estimation-table...')
-      for (const e of estimationsData) {
-        await dynamoDbLib.call('put', {
-          TableName: process.env.tableName,
-          Item: e
-        })
-      }
-    } catch (err) {
-      console.error(err)
-      return failure({ status: false, error: err })
-    }
-  }
-  console.log('Get all price estimations on database...')
   try {
     const result = await dynamoDbLib.call('scan', {
       TableName: process.env.tableName
