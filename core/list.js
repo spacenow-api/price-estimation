61b0c38cd373b334d61d7de49ea3ccb386a4b887
Arthemus <arthemus.moreira@gmail.com>
2019-08-16 12:20:59 +1000

    Cleaning code.

--- core/list.js
+++ core/list.js
@@ -1,17 +1,13 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib"
-import { success, failure } from "../libs/response-lib"
+import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
 
-export const main = async (event, context) => {
-  
-  const params = {
-    TableName: process.env.tableName
-  }
-
+export const main = async () => {
   try {
-    const result = await dynamoDbLib.call("scan", params);
-    return success({count:result.Items.length, results: result.Items})
+    const result = await dynamoDbLib.call('scan', {
+      TableName: process.env.tableName
+    })
+    return success({ count: result.Items.length, results: result.Items })
   } catch (e) {
     return failure({ status: false })
   }
-
 }
