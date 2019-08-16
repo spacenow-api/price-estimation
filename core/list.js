e644eebd3a1ef5694293c434b1c1f34580f4f678
Arthemus <arthemus.moreira@gmail.com>
2019-08-16 14:29:53 +1000

    Testing JSON reading.

--- core/list.js
+++ core/list.js
@@ -1,26 +1,27 @@
-// import * as dynamoDbLib from '../libs/dynamodb-lib'
+import * as dynamoDbLib from '../libs/dynamodb-lib'
 import { success, failure } from '../libs/response-lib'
 
-const estimationsData = require('./../estimations.json')
+const FILL_DATABASE = true
 
 export const main = async () => {
-  try {
-    // Cleaning all records...
-    // await dynamoDbLib.call('delete', { TableName: process.env.tableName })
-
-    // Put all itens on price-estimation-table...
-    for (const e of estimationsData) {
-      await dynamoDbLib.call('put', {
-        TableName: process.env.tableName,
-        Item: e
-      })
+  if (FILL_DATABASE) {
+    const estimationsData = require('./../estimations.json')
+    try {
+      console.log('Cleaning all records...')
+      await dynamoDbLib.call('delete', { TableName: process.env.tableName })
+      console.log('Put all itens on price-estimation-table...')
+      for (const e of estimationsData) {
+        await dynamoDbLib.call('put', {
+          TableName: process.env.tableName,
+          Item: e
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      return failure({ status: false, error: err })
     }
-  } catch (err) {
-    console.error(err)
-    return failure({ status: false, error: err })
   }
-
-  // Get all price estimations on database...
+  console.log('Get all price estimations on database...')
   try {
     const result = await dynamoDbLib.call('scan', {
       TableName: process.env.tableName
